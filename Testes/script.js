92419cd58e296edafa5e9632c03e3bbf4c6fe695
Durvaldo Gonçalves Marques <durvaldomarques@gmail.com>
2022-10-24 16:53:13 -0300

    testes

---
+++ Testes/script.js
@@ -0,0 +1,15 @@
+
+const colors = {
+    black: '#000000',
+    blue: '#00A7E1',
+    white: '#FFFFFF',
+    pink: '#FF2C55',
+    yellow: '#FFAE03'
+}
+
+function verificacor(cor) {
+    console.log(colors.cor)
+}
+
+
+verificacor('white')
