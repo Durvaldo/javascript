d75a27ef2caf2dd2554f3e87d9ac8ee40d99d72d
Durvaldo Gonçalves Marques <durvaldomarques@gmail.com>
2022-10-25 18:21:51 -0300

    Update script.js

--- Testes/script.js
+++ Testes/script.js
@@ -1,15 +1,34 @@
-
-const colors = {
-    black: '#000000',
-    blue: '#00A7E1',
-    white: '#FFFFFF',
-    pink: '#FF2C55',
-    yellow: '#FFAE03'
+const convertToHex = color => {
+    const colors = {
+        black: '#000000',
+        blue: '#00A7E1',
+        white: '#FFFFFF',
+        pink: '#FF2C55',
+        yellow: '#FFAE03'
+    }
+    return colors[color] 
+    ? `O hexadecimal para a cor ${color} é ${colors[color]}`
+    : `Não temos o equivalente hexadecimal para ${color}`
 }
 
-function verificacor(cor) {
-    console.log(colors.cor)
-}
+const colors = [
+    'black',
+    'blue',
+    'white',
+    'azul',
+    'preto'
+]
+
+const logColorMenssage = color => console.log(convertToHex(color))
+colors.forEach(logColorMenssage)
 
 
-verificacor('white')
+const lista = [ {valor: "azul", tamanho: "grande"}, {valor: "verde", tamanho: "pequeno"}, {valor: "branco", tamanho: "medio"} ];
+
+lista.forEach(function(value) {
+    value.medida = value.tamanho;
+    delete value.tamanho;
+});
+
+console.log( lista );
+
