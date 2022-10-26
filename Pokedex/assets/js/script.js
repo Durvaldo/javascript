1ef44f951b5ee0b9c5ee0283e1e9e35bb10744ba
Durvaldo Gonçalves Marques <durvaldomarques@gmail.com>
2022-10-26 11:23:08 -0300

    Update script.js

--- Pokedex/assets/js/script.js
+++ Pokedex/assets/js/script.js
@@ -1,6 +1,6 @@
-const fetchPokemon = () => {
-    const getPokemonUrl = id => `https://pokeapi.co/api/v2/pokemon/${id}`
+const getPokemonUrl = id => `https://pokeapi.co/api/v2/pokemon/${id}`
 
+const fetchPokemon = () => {
     const pokemonPromises = []
 
     for (let id = 1; id <= 151; id++) {
@@ -9,7 +9,25 @@ const fetchPokemon = () => {
     
     Promise.all(pokemonPromises)
         .then(pokemons => {
-            console.log(pokemons)
+
+            const lisPokemons = pokemons.reduce((accumulator, pokemon) => {
+                const types = pokemon.types.map(typeInfo => typeInfo.type.name)
+                accumulator += /*html*/`
+                    <li class="card ${types[0]}">
+                        <img 
+                            class="card-image" 
+                            alt="${pokemon.name}" src="https://cdn.traction.one/pokedex/pokemon/${pokemon.id}.png"
+                        />
+                        <h2 class="card-title">${pokemon.id} . ${pokemon.name}</h2>
+                        <p class='card-subtitle'>${types.join(' | ')}</p>
+                    </li>
+                    `
+                return accumulator
+            }, '')
+
+            const ul = document.querySelector('[data-js="pokedex"]')
+
+            ul.innerHTML = lisPokemons
         })
 } 
 
